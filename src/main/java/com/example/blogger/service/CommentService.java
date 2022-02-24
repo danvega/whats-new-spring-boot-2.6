9ad8dffd0ec77d60be9a3545cfada5fdb8c016d0
Dan Vega <danvega@gmail.com>
2022-02-24 13:34:30 -0500

    using setter injection you can enable circular references

--- src/main/java/com/example/blogger/service/CommentService.java
+++ src/main/java/com/example/blogger/service/CommentService.java
@@ -12,4 +12,5 @@ public class CommentService {
     public void setPostService(PostService postService) {
         this.postService = postService;
     }
+
 }
--- src/main/java/com/example/blogger/service/PostService.java
+++ src/main/java/com/example/blogger/service/PostService.java
@@ -1,15 +1,15 @@
 package com.example.blogger.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostService {
 
-    private final CommentService commentService;
+    private CommentService commentService;
 
-    public PostService(@Lazy CommentService commentService) {
+    @Autowired
+    public void setCommentService(CommentService commentService) {
         this.commentService = commentService;
     }
 
